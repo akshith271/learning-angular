6747195234e332a50643ac9008262cfb34ec5bf8
Akshith <akshithbharadwaj@gmail.com>
2020-04-18 21:38:24 +0530

    Angular Components Part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
 
   providers: [],
   bootstrap: [AppComponent],
 
-  imports : [
+  imports: [
+    BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
-
 })
 export class AppModule { }
