4d21c0c6a348534454e2d8db2e4143ec97c89368
Akshith <akshithbharadwaj@gmail.com>
2020-04-22 12:06:31 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
-import {MatListModule} from "@angular/material/list";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import "hammerjs";
+import { MenuComponent } from "./menu/menu.component";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 
-import {DishService} from "./services/dish.service";
+import { DishService } from "./services/dish.service";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
 
-  providers: [
-    DishService
-  ],
+  providers: [DishService],
   bootstrap: [AppComponent],
 
   imports: [
@@ -34,7 +36,7 @@ import {DishService} from "./services/dish.service";
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
