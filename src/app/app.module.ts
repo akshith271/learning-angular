26789ba7fb01718380295ad7771da559e2a4341b
Akshith <akshithbharadwaj@gmail.com>
2020-04-24 12:57:29 +0530

    SPA part-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 
 import { DishService } from "./services/dish.service";
+import {PromotionService} from "./services/promotion.service";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { DishService } from "./services/dish.service";
     ContactComponent,
   ],
 
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent],
 
   imports: [
