e48e4bab2a992da193d96fc6003ea5fa151bc2f0
Akshith <akshithbharadwaj@gmail.com>
2020-04-24 12:04:27 +0530

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
+
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import "hammerjs";
-import { MenuComponent } from "./menu/menu.component";
 import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import {AppRoutingModule} from "./app-routing/app-routing.module";
 
-import { DishService } from "./services/dish.service";
+import "hammerjs";
+
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+
+import { DishService } from "./services/dish.service";
 
 @NgModule({
   declarations: [
@@ -23,6 +30,10 @@ import { FooterComponent } from "./footer/footer.component";
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
 
   providers: [DishService],
@@ -37,6 +48,7 @@ import { FooterComponent } from "./footer/footer.component";
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule
   ],
 })
 export class AppModule {}
