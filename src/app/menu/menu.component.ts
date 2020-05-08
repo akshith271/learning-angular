2b9b86092e28851dfbd2b71007f9eab5f54d95a4
Akshith <akshithbharadwaj@gmail.com>
2020-05-08 13:31:26 +0530

    Animations Part-2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,11 +3,20 @@ import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
 import { Leader } from "../shared/leader";
 import { LeaderService } from "../services/leader.service";
+import {flyInOut,expand} from "../animations/app.animation";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display : block;'
+  },
+  animations:[
+    flyInOut(),
+    expand()
+  ]
 })
 export class MenuComponent implements OnInit {
   dishes: Dish[];
